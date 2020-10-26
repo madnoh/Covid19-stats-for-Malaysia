111f11c424426c4a5c0d90359be7be0c49a9d6da
Muhammad Noh <madnoh@live.com.sg>
2020-10-26 09:19:14 +0000

    import get and get info as json

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import requests
+from requests import get
 
-r = requests.get(url="https://apicovid2019.herokuapp.com/country=Malaysia")
+r = get(url="https://apicovid2019.herokuapp.com/country=Malaysia").json()
 #print(r.json())  to get all the data
 
-print(f"Covid-19 stats for {r.json()['Country/other']}\n")
-print(f"New cases: {r.json()['NewCases']}")
-print(f"Total cases: {r.json()['TotalCases']}\n")
+print(f"Covid-19 stats for {r['Country/other']}\n")
+print(f"New cases: {r['NewCases']}")
+print(f"Total cases: {r['TotalCases']}\n")
 
-print(f"New recovered: {r.json()['NewRecovered']}")
-print(f"Active cases: {r.json()['ActiveCases']}")
-print(f"Total recoveries: {r.json()['TotalRecovered']}\n")
+print(f"New recovered: {r['NewRecovered']}")
+print(f"Active cases: {r['ActiveCases']}")
+print(f"Total recoveries: {r['TotalRecovered']}\n")
 
-print(f"New deaths: {r.json()['NewDeaths']}")
-print(f"Total deaths: {r.json()['TotalDeaths']}")
+print(f"New deaths: {r['NewDeaths']}")
+print(f"Total deaths: {r['TotalDeaths']}")
